c7ae6c453ece303466dc536006c5a3a021af5db3
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2024-12-25 16:59:06 +0900

    DynamoDB data sources

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -13,12 +13,6 @@ const schema = a.schema({
     })
     .authorization((allow) => [allow.publicApiKey()]),
 
-  Post: a.customType({
-    Division: a.id().required(),
-    DivisionName: a.string().required(),
-    Controller: a.string(),
-    }),
-
 });
 
 export type Schema = ClientSchema<typeof schema>;
