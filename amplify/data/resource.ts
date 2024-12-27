4fdb6c587ea36e68ec08819ef85c75ad765eb0f3
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2024-12-27 15:26:01 +0900

    data

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -9,7 +9,7 @@ specifies that any user authenticated via an API key can "create", "read",
 const schema = a.schema({
   Todo: a
     .model({
-      content: a.string(),
+      content: a.string(),    
     })
     .authorization((allow) => [allow.publicApiKey()]),
 
@@ -28,7 +28,7 @@ export const data = defineData({
   authorizationModes: {
     defaultAuthorizationMode: "apiKey",
     apiKeyAuthorizationMode: {
-      expiresInDays: 30,
+      expiresInDays: 40,
     },
   },
 });
